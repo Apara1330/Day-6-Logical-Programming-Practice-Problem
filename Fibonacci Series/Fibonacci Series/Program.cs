c5c7a16574ebd94b631bf9f8497da6a09428f2f6
Apara1330 <polapara1330@gmail.com>
2022-02-07 18:37:56 +0530

    [Apara]-Logical Programs completed

---
+++ Fibonacci Series/Fibonacci Series/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Fibonacci_Series
+{
+    internal class FibonacciSeries
+    {
+        static void Main(string[] args)
+        {
+            int n1 = 0, n2 = 1, n3, i, number;
+            Console.WriteLine("Enter the number : ");
+            number = int.Parse(Console.ReadLine());
+            Console.WriteLine(n1 + " " );//printing 0
+            for(i=1;i<number;i++)
+            {
+                n3 = n1 + n2;//formula i.e 
+                Console.WriteLine(n3 + " ");
+                n1 = n2;
+                n2 = n3;
+            }
+            Console.ReadLine(); 
+        }
+    }
+}
---
+++ PrimeNumber/PrimeNumber/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PrimeNumber
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num, i, ctr=0;
+            Console.WriteLine("Enter any number : ");
+            num = Convert.ToInt32(Console.ReadLine());
+            for (i = 2; i <= num / 2; i++)
+            {
+                if (num % i == 0)
+                {
+                    ctr++;
+                    break;
+                }
+            }
+            if (ctr == 0 && num != 1)
+                Console.Write("{0} is a prime number.\n", num);
+            else
+                Console.Write("{0} is not a prime number\n", num);
+
+        }
+    }
+}
